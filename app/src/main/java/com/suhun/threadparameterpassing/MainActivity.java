0f09e58ffaad901305c1fec3a4a13b4bf131b680
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-29 13:01:55 +0800

    Initial Textview

--- app/src/main/java/com/suhun/threadparameterpassing/MainActivity.java
+++ app/src/main/java/com/suhun/threadparameterpassing/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView showBroadcast, showMessageSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
-
+    private void initView(){
+        showBroadcast = findViewById(R.id.broadcastResult);
+        showMessageSend = findViewById(R.id.MessageResult);
+    }
     public void runBroadcastPassingFun(View view){
 
     }
