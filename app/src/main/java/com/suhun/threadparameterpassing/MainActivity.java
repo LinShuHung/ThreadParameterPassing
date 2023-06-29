5f3250881e24ecea8fa96270ceae157b3850e3a8
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-29 12:58:45 +0800

    Add layout

--- app/src/main/java/com/suhun/threadparameterpassing/MainActivity.java
+++ app/src/main/java/com/suhun/threadparameterpassing/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.threadparameterpassing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void runBroadcastPassingFun(View view){
+
+    }
+
+    public void runMessagePassingFun(View view){
+
     }
 }
